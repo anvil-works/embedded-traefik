1438f59f3aba08d1e67d1e945e9e4079dfcb013c
Ian Davies <git@iandavies.org>
2024-07-18 08:36:32 +0100

    Add missing dependency

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "AGPL"
             :url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.commons/commons-compress "1.4.1"]
+                 [org.apache.commons/commons-compress "1.26.2"]
+                 [commons-io/commons-io "2.16.1"]
                  [clj-yaml "0.4.0"]])
